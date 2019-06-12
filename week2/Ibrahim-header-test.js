066538eeaf25cdb5f859eaf9916b8e2869f8142f
Alsaddig <36203347+Alsaddig@users.noreply.github.com>
2019-06-12 14:59:44 -0700

    Added Header

---
+++ week2/Ibrahim-header-test.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Alsaddig Ibrahim 
+; Date:   9 june 2019
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('../Ibrahim-header.js');
+
+console.log(header.display("Alsaddig", "Ibrahim", "Week2 Exercise 2.2"));
---
+++ week2/header.js
@@ -0,0 +1,19 @@
+/*
+============================================
+; Title:  header.js
+; Author: Professor Krasso 
+; Date:   June/9/ 2019
+; Modified By: <Alsaddig Ibrahim>
+; Description: Displays a formatted header
+;===========================================
+*/
+
+/**
+* Params: firstName, lastName, assignment
+* Response: output 
+* Description: Returns a well-formatted string header
+*/
+const header = require('../Ibrahim-header.js');
+
+// Output
+console.log(header.display('Alsaddig', 'Ibrahim', 'Exercise 2.2 - Header'));
