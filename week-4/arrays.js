af342e4b1a6a0807b88ea7e4e8ee49c3dee6a12e
Alsaddig <36203347+Alsaddig@users.noreply.github.com>
2019-06-18 16:55:26 -0700

    Added Error Array File

---
+++ week-4/arrays.js
@@ -0,0 +1,23 @@
+/*
+============================================
+; Title:  arrays.js
+; Author: Professor Krasso
+; Date:   25 June 2017
+; Description: Demonstrates how to define and output array values
+;===========================================
+*/
+const header = require('../Ibrahim-header.js');
+
+console.log(header.display("Alsaddig", "Ibrahim", "Ibrahim Error Array"));
+// array of numerical values
+const  fruits["Banana", "Orange", "Apple ", "Mango "];
+
+// iterate over the array and output the results
+console.log('\n-- DISPLAYING Fruits ARRAY --\n')
+
+for (let i = 0; x < fruits.length; x++) {
+  console.log(x+1 + '-  [' + fruits[x]+']')
+
+}
+
+
