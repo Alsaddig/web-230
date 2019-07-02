a5209bdc21a9b96e87ed69e65b0047c86ebb47f4
Alsaddig <36203347+Alsaddig@users.noreply.github.com>
2019-07-02 14:16:35 -0700

    Added Error File

---
+++ week6/Ibrahim-Error.js
@@ -0,0 +1,22 @@
+/*
+============================================
+; Title:  Object.js
+; Author: Alsaddig Ibrahim
+; Date:  July 2 2019
+; Description: Demonstrates how to define and output array values
+;===========================================
+*/
+
+// Start Code
+const header = require('../Ibrahim-header.js');
+
+console.log(header.display("Alsaddig", "Ibrahim", "two Error file"));
+
+var person = {
+  firstName : "Alsaddig",
+  lastName  : "Ibrahim",
+  age     : 37,
+  eyeColor  : "Black"
+};
+console.log("\nMy full Name "+firstName.person+" "+lastName.person);
+console.log("my last name "+lastName+" my age is "+age)
